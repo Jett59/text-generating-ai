683abc70b9ade44d9c317d299bf3f59cad72ffa0
jett59 <jett.thompson.59@gmail.com>
2024-04-20 12:24:20 +1000

    change the positional encoding scheme

--- concept.py
+++ concept.py
@@ -19,15 +19,19 @@ class ConceptLayer(layers.Layer):
         return summed_conceptual_matrix
 
     def call(self, input):
-        input = self.dense(input)
         conceptual_matrices = []
-        summed_positional_preceding_tokens = tf.zeros((input.shape[0], input.shape[-1]), dtype=input.dtype)
         # The first matrix is unique in that there are no preceding tokens, which means that it is always equal to 0.
         conceptual_matrices.append(tf.zeros((input.shape[0], input.shape[-1], input.shape[-1]), dtype=input.dtype))
         for i in range(1, input.shape[-2]):
-            summed_positional_preceding_tokens += input[:, i-1]
-            summed_positional_preceding_tokens /= 1.2 # This makes the positional factor equal to 1/1.2^d, where d is the distance.
-            # This is because the token placed in the list first will be divided over and over again in this loop, giving the geometric pattern of 1/1.2^d.
+            # Each token is multiplied by 1/the distance to the current token.
+            positional_factors = []
+            for j in range(i):
+                positional_factors.append(1 / (i - j))
+            positional_factors = tf.convert_to_tensor(positional_factors, dtype=input.dtype)
+            positional_factors = tf.expand_dims(positional_factors, axis=-1)
+            preceding_tokens = input[:, :i]
+            positionally_encoded_preceding_tokens = preceding_tokens * positional_factors
+            summed_positional_preceding_tokens = tf.reduce_sum(positionally_encoded_preceding_tokens, axis=1)
             conceptual_matrices.append(self.calculate_summed_conceptual_matrix(input[:, i], summed_positional_preceding_tokens))
         conceptual_matrices = tf.stack(conceptual_matrices, axis=1)
         # concept_matrices is in the shape (batch_size, sequence_length, embedding_dimension, embedding_dimension).
@@ -39,4 +43,4 @@ class ConceptLayer(layers.Layer):
         # Add and normalize, then we're done.
         result += input
         result = self.normalize(result)
-        return self.dropout(result)
+        return self.dropout(self.dense(result))
